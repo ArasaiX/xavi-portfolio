5eea24ab560630eed151dc821e19e812c5c53691
Xavi <fernandezxavi1@gmail.com>
2024-01-28 00:37:21 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section>
+            <div>
+                <h1>Portfolio de Xavi Fernández</h1>
+            </div>
+            <div>
+                {/* <a href="#about">Sobre mí</a>
+                <a href="#projects">Proyectos</a> */}
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
