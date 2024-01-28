26a663daf9c83358439a24f9f778a9710f86b63b
Xavi <fernandezxavi1@gmail.com>
2024-01-28 13:09:46 +0100

    add footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,13 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
-            </div>
+        <footer className="footer-container">
+            <section className="pre-footer">
+
+            </section>
+            <section className="footer">
+                
+            </section>
         </footer>
     );
 }
